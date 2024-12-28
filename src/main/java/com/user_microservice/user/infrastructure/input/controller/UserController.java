fdcc8f54608e3dcd4f3f6cfbd886b49404d73d7e
julian98789 <julian_gomez69222@elpoli.edu.co>
2024-12-28 15:28:04 -0500

    Cree documentacion del controlador de usuarios

--- src/main/java/com/user_microservice/user/infrastructure/input/controller/UserController.java
+++ src/main/java/com/user_microservice/user/infrastructure/input/controller/UserController.java
@@ -3,6 +3,11 @@ package com.user_microservice.user.infrastructure.input.controller;
 import com.user_microservice.user.application.dto.user_dto.UserRequest;
 import com.user_microservice.user.application.dto.user_dto.UserResponse;
 import com.user_microservice.user.application.handler.IUserHandler;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
@@ -19,6 +24,20 @@ public class UserController {
 
     private final IUserHandler userHandler;
 
+    @Operation(
+            summary = "Registrar un nuevo usuario",
+            description = "Este endpoint permite registrar un nuevo usuario en el sistema enviando los datos necesarios en el cuerpo de la solicitud.",
+            tags = {"User"}
+    )
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "201", description = "Usuario registrado exitosamente",
+                    content = @Content(mediaType = "application/json",
+                            schema = @Schema(implementation = UserResponse.class))),
+            @ApiResponse(responseCode = "400", description = "Solicitud inválida. Error en la validación de datos.",
+                    content = @Content(mediaType = "application/json")),
+            @ApiResponse(responseCode = "500", description = "Error interno del servidor.",
+                    content = @Content(mediaType = "application/json"))
+    })
     @PostMapping("/crear")
     public ResponseEntity<UserResponse> resgisterUser(@Valid @RequestBody UserRequest userRequest){
         UserResponse registerUser = userHandler.registerUser(userRequest);
